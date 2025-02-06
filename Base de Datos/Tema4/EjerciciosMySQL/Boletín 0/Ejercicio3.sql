a6c53d483533df415312fe5c4bd452b5692b4d21
dbonper <dbonper789@g.educaand.es>
2025-02-06 08:13:43 +0100

    ...

--- Base de Datos/Tema4/EjerciciosMySQL/Boletín 0/Ejercicio3.sql
+++
@@ -1,7 +0,0 @@
-use boletin0;
-
-insert into Alumnos(DNI, Nombre, Apellido1, Apellido2, Sexo)
-values("12345678J", "Sergio", "Navas", "Rental", "H" );
-
-/* No es posible realizar este apartado, pues curso no puede ir vacío*/
-
