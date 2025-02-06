7d495861b9561abf0b0b51829c32f8a6d9402f61
MrKaleth <dariobope@gmail.com>
2025-02-06 09:51:37 +0100

    Corrrecciones en boletin3.Torneo

---
+++ Programación/Códigos Java/Java/.metadata/.plugins/org.eclipse.core.resources/.history/86/803731c166e4001f1bb0b28b9e22ff67
@@ -0,0 +1,27 @@
+package boletin3.Torneo;
+
+public class Participante {
+	String nickname;
+	String nombre;
+	int edad;
+	int puntuacion;
+
+	public Participante(String nickname, String nombre, int edad, int puntuacion) {
+		super();
+		this.nickname = nickname;
+		this.nombre = nombre;
+		this.edad = edad;
+		this.puntuacion = puntuacion;
+	}
+
+	@Override
+	public String toString() {
+		return "Participante [nickname=" + nickname + ", nombre=" + nombre + ", edad=" + edad + ", puntuacion="
+				+ puntuacion + "]";
+	}
+
+	public int obtenPuntucion() {
+		return puntuacion;
+	}
+
+}
--- Programación/Códigos Java/Java/Unidad_3/src/boletin3/Torneo/Participante.java
+++ Programación/Códigos Java/Java/Unidad_3/src/boletin3/Torneo/Participante.java
@@ -1,10 +1,43 @@
 package boletin3.Torneo;
 
 public class Participante {
-	String nickname;
-	String nombre;
-	int edad;
-	int puntuacion;
+	private String nickname;
+	private String nombre;
+	private int edad;
+	private int puntuacion;
+
+	
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public int getPuntuacion() {
+		return puntuacion;
+	}
+
+	public void setPuntuacion(int puntuacion) {
+		this.puntuacion = puntuacion;
+	}
 
 	public Participante(String nickname, String nombre, int edad, int puntuacion) {
 		super();
