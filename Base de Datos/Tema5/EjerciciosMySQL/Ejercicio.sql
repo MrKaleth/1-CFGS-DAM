52f6cd2ba003e3aec140b98001d281a49cf7e6bd
MrKaleth <dariobope@gmail.com>
2025-02-14 08:17:05 +0100

    Cambios en sql

--- Base de Datos/Tema5/EjerciciosMySQL/Ejercicio.sql
+++ Base de Datos/Tema5/EjerciciosMySQL/Ejercicio.sql
@@ -9,9 +9,31 @@ grant 'Admin_C' to 'Administrador'@'localhost';
 show grants for 'Administrador'@'localhost';
 
 create role 'Admin_D';
-grant create on *.* to 'Admin_D' with grant option;
+grant all privileges on *.* to 'Admin_D' with grant option;
 create user 'Administrador2'@'localhost' identified by 'pass_admin';
 grant 'Admin_D' to 'Administrador2'@'localhost';
+SET DEFAULT ROLE 'Admin_D' TO 'Administrador2'@'localhost';
 show grants for 'Administrador2'@'localhost';
 
 -- Ejercicio 3 --
+/* Hecho */
+
+-- Ejercicio 4 --
+select user from mysql. user;
+select user();
+
+-- Ejercicio 5 --
+show grants;
+
+-- Ejercicio 6 --
+create table Alumno (
+    Identificador int primary key auto_increment,
+    Nombre varchar(100) not null
+);
+
+insert into Alumno (Nombre) values
+('Juan Pérez'),
+('María Gómez'),
+('Carlos López'),
+('Ana Torres'),
+('Luis Fernández');
