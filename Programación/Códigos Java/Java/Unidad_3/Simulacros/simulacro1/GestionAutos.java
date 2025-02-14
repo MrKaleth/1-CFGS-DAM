32b4d3824478696053599bfd4519043b342920a0
MrKaleth <dariobope@gmail.com>
2025-02-14 10:07:06 +0100

    Simulacro 1 Añadido

---
+++ Programación/Códigos Java/Java/Unidad_3/Simulacros/simulacro1/GestionAutos.java
@@ -0,0 +1,13 @@
+package simulacro1;
+
+public class GestionAutos {
+
+	public static void main(String[] args) {
+
+		Conductor c1 = new Conductor("Antonio", -5);
+		
+		System.out.println(c1);
+		
+	}
+
+}
