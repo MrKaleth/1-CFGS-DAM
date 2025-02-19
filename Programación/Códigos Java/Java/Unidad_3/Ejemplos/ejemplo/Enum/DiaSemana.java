cc88ae6d7aafa075dee8bb1f0ea688303ec002ae
MrKaleth <dariobope@gmail.com>
2025-02-19 08:25:28 +0100

    Cambios en los ejemplos de los ENUM

--- Programación/Códigos Java/Java/Unidad_3/Ejemplos/ejemplo/Enum/DiaSemana.java
+++ Programación/Códigos Java/Java/Unidad_3/Ejemplos/ejemplo/Enum/DiaSemana.java
@@ -4,4 +4,6 @@ public enum DiaSemana {
 
 	LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, SABADO, DOMINGO;
 
+	
+	
 }
--- Programación/Códigos Java/Java/Unidad_3/Ejemplos/ejemplo/Enum/Reserva.java
+++ Programación/Códigos Java/Java/Unidad_3/Ejemplos/ejemplo/Enum/Reserva.java
@@ -11,8 +11,16 @@ public class Reserva {
 
 	public static void main(String[] args) {
 
-		Mes mes2 = Mes.valueOf("VERANO");
-		System.out.println(mes2);
+		/* Mes mes2 = Mes.valueOf("VERANO");
+		System.out.println(mes2); */
+		
+		for(DiaSemana dia : DiaSemana.values()) {
+			System.out.println(dia);
+			if(dia == DiaSemana.MARTES) {
+				System.out.println("Hoy es martes amigos");
+				
+			}
+		}
 
 	}
 
