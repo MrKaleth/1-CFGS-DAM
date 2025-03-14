4444d2388a995abb193b42065c6d2dff19b5d0f1
MrKaleth <dariobope@gmail.com>
2025-03-14 12:23:44 +0100

    Añadida la zona para las Excepciones

---
+++ Programación/Códigos Java/Java/Unidad_3/Excepciones/boletin1/Ejercicio1/Actividad1.java
@@ -0,0 +1,22 @@
+package boletin1.Ejercicio1;
+
+public class Actividad1 {
+	
+	public static void main(String[] args) {
+		// Declaro objeto fichero
+		File fichero = new File("salida.txt");
+		// Lo creo si no existe
+		// boolean creado = fichero.createNewFile();
+	}
+
+}
+
+class File{
+	private String Hola;
+
+	public File(String hola) {
+		super();
+		Hola = hola;
+	}
+	
+}
