509442fa07ee13ad9296debc8a64b28eaf9a358f
MrKaleth <dariobope@gmail.com>
2025-03-14 12:23:44 +0100

    Añadida la zona para las Excepciones

---
+++ Programación/Códigos Java/Java/Unidad_3/Excepciones/boletin1/Ejercicio2/Ejercicio2.java
@@ -0,0 +1,11 @@
+package boletin1.Ejercicio2;
+
+public class Ejercicio2 {
+	public static void main(String[] args) {
+		m1();
+	}
+
+	public static boolean m1() throws NullPointerException {
+		throw new NullPointerException("Genero NullPointer");
+	}
+}
